85687400d461f668369a6207769b692bd6c0b571
Lescano10 <fellipelescano10@gmail.com>
2022-05-03 15:55:43 -0300

    aula 8 PDO
    PDO adição

---
+++ dados.php
@@ -0,0 +1,51 @@
+<<?php
+
+$servicos = array(
+    (object) [
+        'classe' => 'bi-collection',
+        'titulo' => 'log 1',
+        'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'
+    ],
+    (object) [
+        'classe' => 'bi-building',
+        'titulo' => 'log 2',
+        'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'
+    ],
+    (object) [
+        'classe' => 'bi-toggles2',
+        'titulo' => 'log 3',
+        'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'
+    ],
+    (object) [
+        'classe' => 'bi-toggles2',
+        'titulo' => 'log 4',
+        'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'
+    ]
+);
+$blogs = array(
+
+    (object) [
+        'classe' => 'Pasta',
+        'titulo' => 'Home',
+        'descricao' => 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+        'nome' => 'Bic Pay',
+        'data' => 'Setembro, 2023 &middot; 8 min read'
+    ],
+    (object) [
+        'classe' => 'Meio',
+        'titulo' => 'Home post',
+        'descricao' => 'This text is a bit longer to illustrate the adaptive height of each card. Some quick example text to build on the card title and make up the bulk of the cards content.',
+        'nome' => 'Josiah Barclay',
+        'data' => 'Fevereiro, 2023 &middot; 12 min read'
+    ],
+    (object) [
+        'classe' => 'Pasta',
+        'titulo' => 'Home last post',
+        'descricao' => 'Some more quick example text to build on the card title and make up the bulk of the cards content.',
+        'nome' => 'Evelyn Martinez',
+        'data' => 'Junho 3, 2023 &middot; 15 min read'
+    ]
+);
+
+?>
+
